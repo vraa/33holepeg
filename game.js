0179f9f8138590ce580fe02c300d6acc6afe6ac7
Veera <veera.k.sundar@gmail.com>
2014-03-18 18:58:16 +0530

    Add help text, 3rd party libs and few more styles.

--- game.js
+++ game.js
@@ -1,163 +1,174 @@
 (function($){
 
-	var BOARD_SIZE = 7;
-	var board;
-	var moves = 0;
+	function Game(){
 
-	function init(){
-		createBoard();
-		renderBoard();
-		$("#restart").click(restartGame);
-	}
 
-	function restartGame(){
-		if(confirm('Restart game? Current progress will be lost.')){
-			moves = 0;
+		var BOARD_SIZE = 7;
+		var board;
+		var moves = 0;
+
+		function init(){
 			createBoard();
 			renderBoard();
+			$("#restart, #newGame").click(restartGame);
 		}
-	}
 
-	function createBoard(){
-		var i,j;
-		board = new Array(BOARD_SIZE);
-		for(i=0; i<BOARD_SIZE; i++){
-			board[i] = new Array(BOARD_SIZE);
-			for(j=0; j<BOARD_SIZE; j++){
-				board[i][j] = isCorner(i,j) ? -1 : 1;
+		function restartGame(){
+			if(confirm('Restart game?')){
+				moves = 0;
+				createBoard();
+				renderBoard();
+				$('.gameover').addClass('hidden');
 			}
 		}
-		board[3][3] = 0;
-	}
 
-	function checkGameStatus(){
-		if(isGameOver()){
-			alert('Game Over');
-		}else{
-			console.log('Not over yet');
-		}
-	}
-
-	function updateScore(){
-		$('.scoreboard').empty().append($('<div>').attr('id', 'moves').addClass('swing animated').html(moves));
-	}
-
-	function isCorner(i,j){
-		var lEdge = 2, rEdge = BOARD_SIZE - 3;
-		return( 
-			(i < lEdge && j < lEdge) || // top-left
-			(i > rEdge && j < lEdge) || // bottom-left
-			(i < lEdge && j > rEdge) || // top-right
-			(i > rEdge && j > rEdge)
-		);
-	}
-
-
-	function renderBoard(){
-		var boardElm = $('#board');
-		boardElm.empty();
-		var i,j, cellClass;
-		for(i=0; i<BOARD_SIZE; i++){
-			var rowElm = $('<div>').addClass('row');
-			for(j=0; j<BOARD_SIZE; j++){
-				var id = 'x'+ i + '-' + 'y' + j;
-				var cellElm = $('<div>').addClass('cell').attr('id', id);
-				cellElm.data('x', i).data('y', j);
-				cellElm.empty().append($('<span>').addClass('marble'));
-				switch(board[i][j]){
-					case -1:
-						cellClass = 'dead';
-						break;
-					case 0:
-						cellClass = 'hole';
-						break;
-					case 1:
-						cellClass = "alive";
-						break;
-				}
-				cellElm.addClass(cellClass);
-				rowElm.append(cellElm);
-			}
-			boardElm.append(rowElm);
-		}
-		checkGameStatus();
-		$('.marble').draggable({
-			containment: "#board",
-			revert: "invalid",
-			start : function(){
-				$(this).css('z-index', 10);
-				var cellElm = $(this).parent('.cell');
-				var cX = cellElm.data('x');
-				var cY = cellElm.data('y');
-				var holes = getAvailableHoles(cX, cY);
-				$.each(holes, function(i,hole){
-					var holeId  = 'x' + hole.x + '-' + 'y' + hole.y;
-					$('#' + holeId + '.hole').droppable({
-						drop : function(){
-							moveMarble($(this).data('x'), $(this).data('y'), cX, cY);
-						}
-					}).addClass('target');
-				});
-			},
-			stop : function(){
-				$('.cell.target').removeClass('target');
-			}
-		});
-		updateScore();
-	}
-
-	function moveMarble(nX, nY, cX, cY){
-		board[nX][nY] = 1;
-		board[cX][cY] = 0;
-		moves += 1;
-		var mX, mY;
-		if( cX == nX){
-			mX = cX;
-			mY = Math.min(cY,nY) + 1;
-		}else{
-			mX = Math.min(cX, nX) + 1;
-			mY = cY;
-		}
-		board[mX][mY] = 0;
-		$('#x' + mX + '-y' + mY).find('.marble').animate({
-			height: 'toggle',
-			width: 'toggle'
-		}, 300, renderBoard);
-	}
-
-	function getAvailableHoles(x,y){
-		var holes = [];
-		isCellExists(x, y-2) && board[x][y-2] == 0 && board[x][y-1] == 1 ? holes.push({x:x, y: y-2}) : null;
-		isCellExists(x, y+2) && board[x][y+2] == 0 && board[x][y+1] == 1 ? holes.push({x:x, y: y+2}) : null;
-		isCellExists(x-2, y) && board[x-2][y] == 0 && board[x-1][y] == 1 ? holes.push({x:x-2, y: y}) : null;
-		isCellExists(x+2, y) && board[x+2][y] == 0 && board[x+1][y] == 1 ? holes.push({x:x+2, y: y}) : null;
-		return holes;
-	}
-
-
-	function isCellExists(x,y){
-		var val;
-		try{
-			val = board[x][y];
-		}catch(e){
-			return false;
-		}
-		return true;
-	}
-
-	function isGameOver(){
-		var i,j;
-		for(i=0; i < BOARD_SIZE; i++){
-			for(j=0; j < BOARD_SIZE; j++){
-				if(board[i][j] == 1 && getAvailableHoles(i,j).length > 0){
-					return false;
+		function createBoard(){
+			var i,j;
+			board = new Array(BOARD_SIZE);
+			for(i=0; i<BOARD_SIZE; i++){
+				board[i] = new Array(BOARD_SIZE);
+				for(j=0; j<BOARD_SIZE; j++){
+					board[i][j] = isCorner(i,j) ? -1 : 1;
 				}
 			}
+			board[3][3] = 0;
 		}
-		return true;
+
+		function checkGameStatus(){
+			if(isGameOver()){
+				setTimeout(function(){
+					$('.gameover').removeClass('hidden');
+				}, 500);
+				
+			}
+		}
+
+		function updateScore(){
+			$('.scoreboard').empty().append($('<div>').attr('id', 'moves').addClass('swing animated').html(moves));
+		}
+
+		function isCorner(i,j){
+			var lEdge = 2, rEdge = BOARD_SIZE - 3;
+			return( 
+				(i < lEdge && j < lEdge) || // top-left
+				(i > rEdge && j < lEdge) || // bottom-left
+				(i < lEdge && j > rEdge) || // top-right
+				(i > rEdge && j > rEdge)
+			);
+		}
+
+
+		function renderBoard(){
+			var boardElm = $('#board');
+			boardElm.empty();
+			var i,j, cellClass;
+			for(i=0; i<BOARD_SIZE; i++){
+				var rowElm = $('<div>').addClass('row');
+				for(j=0; j<BOARD_SIZE; j++){
+					var id = 'x'+ i + '-' + 'y' + j;
+					var cellElm = $('<div>').addClass('cell').attr('id', id);
+					cellElm.data('x', i).data('y', j);
+					cellElm.empty().append($('<span>').addClass('marble'));
+					switch(board[i][j]){
+						case -1:
+							cellClass = 'dead';
+							break;
+						case 0:
+							cellClass = 'hole';
+							break;
+						case 1:
+							cellClass = "alive";
+							break;
+					}
+					cellElm.addClass(cellClass);
+					rowElm.append(cellElm);
+				}
+				boardElm.append(rowElm);
+			}
+			checkGameStatus();
+			$('.cell .marble').draggable({
+				containment: "#board",
+				revert: "invalid",
+				start : function(){
+					$(this).css('z-index', 10);
+					var cellElm = $(this).parent('.cell');
+					var cX = cellElm.data('x');
+					var cY = cellElm.data('y');
+					var holes = getAvailableHoles(cX, cY);
+					$.each(holes, function(i,hole){
+						var holeId  = 'x' + hole.x + '-' + 'y' + hole.y;
+						$('#' + holeId + '.hole').droppable({
+							drop : function(){
+								moveMarble($(this).data('x'), $(this).data('y'), cX, cY);
+							}
+						}).addClass('target');
+					});
+				},
+				stop : function(){
+					$('.cell.target').removeClass('target');
+				}
+			});
+			updateScore();
+		}
+
+		function moveMarble(nX, nY, cX, cY){
+			board[nX][nY] = 1;
+			board[cX][cY] = 0;
+			moves += 1;
+			var mX, mY;
+			if( cX == nX){
+				mX = cX;
+				mY = Math.min(cY,nY) + 1;
+			}else{
+				mX = Math.min(cX, nX) + 1;
+				mY = cY;
+			}
+			board[mX][mY] = 0;
+			$('#x' + mX + '-y' + mY).find('.marble').animate({
+				height: 'toggle',
+				width: 'toggle'
+			}, 300, renderBoard);
+		}
+
+		function getAvailableHoles(x,y){
+			var holes = [];
+			isCellExists(x, y-2) && board[x][y-2] == 0 && board[x][y-1] == 1 ? holes.push({x:x, y: y-2}) : null;
+			isCellExists(x, y+2) && board[x][y+2] == 0 && board[x][y+1] == 1 ? holes.push({x:x, y: y+2}) : null;
+			isCellExists(x-2, y) && board[x-2][y] == 0 && board[x-1][y] == 1 ? holes.push({x:x-2, y: y}) : null;
+			isCellExists(x+2, y) && board[x+2][y] == 0 && board[x+1][y] == 1 ? holes.push({x:x+2, y: y}) : null;
+			return holes;
+		}
+
+
+		function isCellExists(x,y){
+			var val;
+			try{
+				val = board[x][y];
+			}catch(e){
+				return false;
+			}
+			return true;
+		}
+
+		function isGameOver(){
+			var i,j;
+			for(i=0; i < BOARD_SIZE; i++){
+				for(j=0; j < BOARD_SIZE; j++){
+					if(board[i][j] == 1 && getAvailableHoles(i,j).length > 0){
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+
+
+		return {
+			init : init
+		}
 	}
 
-	$(init);
+	$(Game().init);
 	
 
 }(jQuery));
